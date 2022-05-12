894cae7120bd7d379ba20c76f6a6f6d4888ad3d6
sungbohsun <sungbohsun@gmail.com>
2022-05-12 10:38:59 +0800

    sung

---
+++ main.py
@@ -0,0 +1,213 @@
+import time
+import pywintypes
+import argparse
+from torch.types import Number
+import win32gui, win32ui, win32con, win32api
+from PIL import Image
+from mss import mss
+import numpy as np
+import cv2
+import torch
+import torch.backends.cudnn as cudnn
+import keyboard
+from ctypes import *
+from models.experimental import attempt_load
+from utils.datasets import LoadStreams, LoadImages
+from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
+    scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path, save_one_box
+from utils.plots import colors, plot_one_box
+from utils.torch_utils import select_device, load_classifier, time_synchronized
+import argparse
+import time
+from pathlib import Path
+
+
+def move_mouse(x, y):
+
+    windll.user32.mouse_event(
+        c_uint(0x0001),
+        c_uint(x),
+        c_uint(y),
+        c_uint(0),
+        c_uint(0)
+    )
+
+def letterbox(img, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleFill=False, scaleup=True, stride=32):
+    # Resize and pad image while meeting stride-multiple constraints
+    shape = img.shape[:2]  # current shape [height, width]
+    if isinstance(new_shape, int):
+        new_shape = (new_shape, new_shape)
+
+    # Scale ratio (new / old)
+    r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+    if not scaleup:  # only scale down, do not scale up (for better test mAP)
+        r = min(r, 1.0)
+
+    # Compute padding
+    ratio = r, r  # width, height ratios
+    new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
+    dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
+    if auto:  # minimum rectangle
+        dw, dh = np.mod(dw, stride), np.mod(dh, stride)  # wh padding
+    elif scaleFill:  # stretch
+        dw, dh = 0.0, 0.0
+        new_unpad = (new_shape[1], new_shape[0])
+        ratio = new_shape[1] / shape[1], new_shape[0] / shape[0]  # width, height ratios
+
+    dw /= 2  # divide padding into 2 sides
+    dh /= 2
+
+    if shape[::-1] != new_unpad:  # resize
+        img = cv2.resize(img, new_unpad, interpolation=cv2.INTER_LINEAR)
+    top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
+    left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
+    img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
+    return img, ratio, (dw, dh)
+
+
+def is_mouse_down():    # Returns true if the left mouse button is pressed
+    lmb_state = win32api.GetKeyState(0x01)
+    return lmb_state < 0
+
+
+@torch.no_grad()
+def detect(opt):
+    source, weights, view_img, save_txt, imgsz = opt.source, opt.weights, opt.view_img, opt.save_txt, opt.img_size
+    # save_img = not opt.nosave and not source.endswith('.txt')  # save inference images
+    webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
+        ('rtsp://', 'rtmp://', 'http://', 'https://'))
+
+    # Directories
+    save_dir = increment_path(Path(opt.project) / opt.name, exist_ok=opt.exist_ok)  # increment run
+    (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
+
+    # Initialize
+    set_logging()
+    device = select_device(opt.device)
+    half = device.type != 'cpu'  # half precision only supported on CUDA
+
+    # Load model
+    model = attempt_load(weights, map_location=device)  # load FP32 model
+    stride = int(model.stride.max())  # model stride
+    imgsz = check_img_size(imgsz, s=stride)  # check img_size
+    # names = model.module.names if hasattr(model, 'module') else model.names  # get class names
+    if half:
+        model.half()  # to FP16
+
+    # Second-stage classifier
+    classify = False
+    if classify:
+        modelc = load_classifier(name='resnet101', n=2)  # initialize
+        modelc.load_state_dict(torch.load('weights/resnet101.pt', map_location=device)['model']).to(device).eval()
+
+    enabled = False
+    toggle_button = 'num lock'
+    enabled = False
+
+    key_down = keyboard.is_pressed('num lock')
+
+    # Some prints for startup
+    print("Anti-recoil script started!")
+    if enabled:
+        print("Currently ENABLED")
+    else:
+        print("Currently DISABLED")
+
+    last_state = False
+    sct = mss()
+    while True:
+        key_down = keyboard.is_pressed(toggle_button)
+        # If the toggle button is pressed, toggle the enabled value and print
+        if key_down != last_state:
+            last_state = key_down
+            if last_state:
+                enabled = not enabled
+                if enabled:
+                    print("Anti-recoil ENABLED")
+                else:
+                    print("Anti-recoil DISABLED")
+
+        if  enabled : 
+            hwnd = win32gui.FindWindow(None,'Apex Legends')
+            # hwnd = win32gui.FindWindow(None,'(95) NEWCASTLE is INSANELY OP in Apex legends - YouTube - Google Chrome')
+            left_x, top_y, right_x, bottom_y = win32gui.GetWindowRect(hwnd)
+            #screen = np.array(ImageGrab.grab( bbox = (left_x, top_y, right_x, bottom_y ) ) )
+            bbox = {'top': top_y, 'left': left_x, 'width': right_x-left_x, 'height':bottom_y-top_y }
+
+            sct_img = sct.grab(bbox) 
+            screen = Image.frombytes("RGB", sct_img.size, sct_img.bgra, "raw", "BGRX")
+
+            # Run inference
+            if device.type != 'cpu':
+                model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
+            # t0 = time.time()
+            img0 = np.array(screen)#.transpose(1, 0, 2)
+
+
+            # Padded resize
+            img = letterbox(img0, 640, stride=32)[0]
+
+
+            # Convert
+            img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
+            img = np.ascontiguousarray(img)
+                
+            img = torch.from_numpy(img).to(device)
+            img = img.half() if half else img.float()  # uint8 to fp16/32
+            img /= 255.0  # 0 - 255 to 0.0 - 1.0
+            if img.ndimension() == 3:
+                img = img.unsqueeze(0)
+
+            # Inference
+            # t1 = time_synchronized()
+            pred = model(img, augment=opt.augment)[0]
+
+            # Apply NMS
+            pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, opt.classes, opt.agnostic_nms,
+                                    max_det=opt.max_det)
+            # t2 = time_synchronized()
+            print(pred)
+            # Apply Classifier
+            if classify:
+                pred = apply_classifier(pred, modelc, img, img0)
+
+            # Process detections
+            for i, det in enumerate(pred):  # detections per image
+                if len(det):
+                    # Rescale boxes from img_size to im0 size
+                    det[:, :4] = scale_coords(img.shape[2:], det[:, :4], img0.shape).round()
+                    x = (det[:,:4][0][0]+det[:,:4][0][2])/2
+                    y = det[:,:4][0][1]+(det[:,:4][0][3]-det[:,:4][0][1])/5
+                    move_mouse(int(x)-1920+960,int(y)-1080+540)
+                    time.sleep(0.01)
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', nargs='+', type=str, default='weights/apexSt1.pt', help='model.pt path(s)')
+    parser.add_argument('--source', type=str, default='game.jpg', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--img-size', type=int, default=1280, help='inference size (pixels)')
+    parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
+    parser.add_argument('--max-det', type=int, default=1000, help='maximum number of detections per image')
+    parser.add_argument('--device', default='0', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--view-img', action='store_true', help='display results')
+    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+    parser.add_argument('--save-crop', action='store_true', help='save cropped prediction boxes')
+    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
+    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
+    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+    parser.add_argument('--augment', action='store_true', help='augmented inference')
+    parser.add_argument('--update', action='store_true', help='update all models')
+    parser.add_argument('--project', default='runs/detect', help='save results to project/name')
+    parser.add_argument('--name', default='exp', help='save results to project/name')
+    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+    parser.add_argument('--line-thickness', default=3, type=int, help='bounding box thickness (pixels)')
+    parser.add_argument('--hide-labels', default=False, action='store_true', help='hide labels')
+    parser.add_argument('--hide-conf', default=False, action='store_true', help='hide confidences')
+    opt = parser.parse_args()  
+    detect(opt)
+
+if __name__ == "__main__":
+    main()  
